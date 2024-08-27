83c1eef40863a98b7bc0b612b3ddac3db109a2c0
Ariana Jorgensen <ariana@arianajorgensen.com>
2024-08-27 14:25:04 -0700

    fixing versioning issues

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useForm, ValidationError } from '@formspree/react';
+import { useForm } from '@formspree/react';
 import "../css/form.css";
 
 const Form = () => {
